4faab571629c268db603db037b72c862a68dfec7
tchwojkofrank <tom@chwojkofrank.com>
2022-12-01 11:48:16 -0800

    base main

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func readInput(fname string) string {
+	content, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Convert []byte to string and print to screen
+	return string(content)
+}
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("Enter input file name.\n")
+		return
+	}
+	params := os.Args[1]
+	inputName := strings.Split(params, " ")[0]
+	fmt.Printf("inputName = %v\n", inputName)
+	text := readInput(inputName)
+	run(text)
+}
+
+func run(input string) {
+}

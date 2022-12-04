98f8a1049d11c007db9b2f8f43e6b0dc6f07a2a9
tchwojkofrank <tom@chwojkofrank.com>
2022-12-03 22:31:30 -0800

    Day 4 solution

--- 04/main.go
+++ 04/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -29,5 +30,59 @@ func main() {
 	run(text)
 }
 
+type Range struct {
+	min int
+	max int
+}
+
+type RangePair [2]Range
+
+func getRange(rangeString string) Range {
+	valueStrings := strings.Split(rangeString, "-")
+	var r Range
+	r.min, _ = strconv.Atoi(valueStrings[0])
+	r.max, _ = strconv.Atoi(valueStrings[1])
+	return r
+}
+
+func getRangePairs(input string) []RangePair {
+	pairStrings := strings.Split(input, "\n")
+	rangePairs := make([]RangePair, len(pairStrings))
+	for i, ps := range pairStrings {
+		rangeStrings := strings.Split(ps, ",")
+		rangePairs[i][0] = getRange(rangeStrings[0])
+		rangePairs[i][1] = getRange(rangeStrings[1])
+	}
+	return rangePairs
+}
+
+func isContaining(rp RangePair) bool {
+	return (((rp[0].min <= rp[1].min) && (rp[0].max >= rp[1].max)) ||
+		((rp[0].min >= rp[1].min) && (rp[0].max <= rp[1].max)))
+}
+
+func isOverlapping(rp RangePair) bool {
+	return (rp[0].min >= rp[1].min && rp[0].min <= rp[1].max) || (rp[0].max >= rp[1].min && rp[0].max <= rp[1].max) ||
+		(rp[1].min >= rp[0].min && rp[1].min <= rp[0].max) || (rp[1].max >= rp[0].min && rp[1].max <= rp[0].max)
+}
+
+func countCondition(rangePairs []RangePair, condition func(rp RangePair) bool) int {
+	count := 0
+
+	for _, rp := range rangePairs {
+
+		if condition(rp) {
+			count++
+		}
+	}
+
+	return count
+}
+
 func run(input string) {
+	rangePairs := getRangePairs(input)
+	contains := countCondition(rangePairs, isContaining)
+	fmt.Printf("Total contains = %d\n", contains)
+	overlaps := countCondition(rangePairs, isOverlapping)
+	fmt.Printf("Total overlaps = %d\n", overlaps)
 }

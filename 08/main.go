6c4d25d1dde1959a39047a36437f7a002fbc739a
tchwojkofrank <tom@chwojkofrank.com>
2022-12-07 17:16:08 -0800

    Day 8 init

---
+++ 08/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func readInput(fname string) string {
+	content, err := os.ReadFile(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Convert []byte to string
+	return string(content)
+}
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("Enter input file name.\n")
+		return
+	}
+	params := os.Args[1]
+	inputName := strings.Split(params, " ")[0]
+	text := readInput(inputName)
+	run(text)
+}
+
+func run(input string) {
+}

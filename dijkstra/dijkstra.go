516f640951fee80d575b1b588ff7e713766b90f5
tchwojkofrank <tom@chwojkofrank.com>
2022-12-12 13:59:24 -0800

    clean

--- dijkstra/dijkstra.go
+++ dijkstra/dijkstra.go
@@ -9,28 +9,6 @@ type Node interface {
 	Neighbors() ([]Node, []int)
 }
 
-/*
-function Dijkstra(Graph, source):
- 2
- 3      for each vertex v in Graph.Vertices:
- 4          dist[v] ← INFINITY
- 5          prev[v] ← UNDEFINED
- 6          add v to Q
- 7      dist[source] ← 0
- 8
- 9      while Q is not empty:
-10          u ← vertex in Q with min dist[u]
-11          remove u from Q
-12
-13          for each neighbor v of u still in Q:
-14              alt ← dist[u] + Graph.Edges(u, v)
-15              if alt < dist[v]:
-16                  dist[v] ← alt
-17                  prev[v] ← u
-18
-19      return dist[], prev[]
-*/
-
 func GetShortestPath(g []Node, start Node, target Node) []Node {
 	distances := make(map[Node]int, 0)
 	prev := make(map[Node]Node, 0)
@@ -119,12 +97,3 @@ func GetShortestDistances(g []Node, start Node) (map[Node]int, map[Node]Node) {
 
 	return distances, prev
 }
-
-/*
-1  S ← empty sequence
-2  u ← target
-3  if prev[u] is defined or u = source:          // Do something only if the vertex is reachable
-4      while u is defined:                       // Construct the shortest path with a stack S
-5          insert u at the beginning of S        // Push the vertex onto the stack
-6          u ← prev[u]                           // Traverse from target to source
-*/
